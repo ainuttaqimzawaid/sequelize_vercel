73acad0401a286811a6cdde6b51761f69cdb0fe9
ainuttaqimzawait <ainuttaqimzawaid1000@gmail.com>
2025-10-04 10:51:49 +0700

    m

--- app.js
+++ app.js
@@ -1,33 +1,9 @@
 const express = require('express');
-const { Sequelize } = require('sequelize');
 
-// 👇 Tambahkan baris ini supaya Vercel tidak tree-shake mysql2
-require('mysql2');
 
 // Inisialisasi Express
 const app = express();
 
-// Sequelize (pakai ENV, bukan hardcode)
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql'
-    }
-);
-
-// Cek koneksi database
-(async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('✅ Database connected successfully');
-    } catch (error) {
-        console.error('❌ Database connection failed:', error);
-    }
-})();
 
 // Route utama
 app.get('/', (req, res) => {
