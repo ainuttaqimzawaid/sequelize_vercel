264b7daf23db0f87e7e2e15c7e8804bec94c42a0
ainuttaqimzawait <ainuttaqimzawaid1000@gmail.com>
2025-10-04 10:38:32 +0700

    m

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Sequelize } = require('sequelize');
 
 // 👇 Tambahkan baris ini supaya Vercel tidak tree-shake mysql2
-// require('mysql2');
+require('mysql2');
 
 // Inisialisasi Express
 const app = express();
